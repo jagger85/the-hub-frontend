0cef9931b2ec6e702e881d7df930ab1208f8e441
Jagger85 <jagger85.crypto@gmail.com>
2023-04-04 14:07:49 +0800

    <-❖ Initial commit ❖->

---
+++ p3-react-app/src/components/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component{
+  render() {
+    return (
+      <div>
+        <h1>HAPPY HACKING!</h1>
+      </div>
+    )
+  }
+}
+export default App;
+
