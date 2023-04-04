abba9f64b4c3217649af471a931af8f330ac728a
Jagger85 <jagger85.crypto@gmail.com>
2023-04-04 14:34:57 +0800

    FEAT: Added top bar

--- p3-react-app/src/layouts/MainLayout.js
+++ p3-react-app/src/layouts/MainLayout.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Drawer, Typography } from "@mui/material";
+import { Drawer, Typography, AppBar, Toolbar} from "@mui/material";
 const drawerWidth = 240;
 
 const styles = {
@@ -21,6 +21,15 @@ function MainLayout({ children }) {
       <Drawer sx={styles.drawer} variant="permanent" anchor="left">
         <Typography variant="h5">THE HUB</Typography>
       </Drawer>
+      <AppBar 
+      position='fixed'
+      elevation={0}>
+      <Toolbar>
+        <Typography>THE HUB</Typography>
+      </Toolbar>
+      </AppBar>
+      <Toolbar/> {/** empty toolbar so the content does not hide beneath */}
+   
     <div>
       {children}
       </div>
