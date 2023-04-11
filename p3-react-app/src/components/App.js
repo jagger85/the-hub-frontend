8c0789f60cda29d937871d98f2390a6a39131211
Jagger85 <jagger85.crypto@gmail.com>
2023-04-11 15:19:25 +0800

    FEAT: Implemented context for wallet, network, endpoint

--- p3-react-app/src/components/App.js
+++ p3-react-app/src/components/App.js
@@ -1,4 +1,5 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { useState, createContext } from 'react';
 import MainLayout from '../layouts/MainLayout';
 import { Routes, Route } from 'react-router-dom';
 import Dashboard from './Dashboard';
@@ -8,20 +9,28 @@ import Collections from './Collections';
 import Marketplace from './Marketplace';
 import Settings from './Settings';
 
-class App extends Component{
-  render() {
+export const AppContext = createContext()
+
+
+const App = () => {
+
+  const [ endpoint, setEndpoint ] = useState('http://ultra.api.eosnation.io');
+  const [network, setNetwork ] = useState('mainnet')
+  const [wallet, setWallet ] = useState('1234')
     return (
-      <Routes>
-        <Route path="/dashboard" element={<Dashboard/>}/>
-        <Route path='/inventory' element={<Inventory/>}/>
-        <Route path='/marketplace' element={<Marketplace/>}/>
-        <Route path='/games' element={<Games/>}/>
-        <Route path='/collections' element={<Collections/>}/>
-        <Route path='/settings' element={<Settings/>}/>
-      </Routes>
-      
+      <AppContext.Provider value={{wallet,network,endpoint}}>
+       <MainLayout>
+          <Routes>
+            <Route path="/" element={<Dashboard/>}/>
+            <Route path='/inventory' element={<Inventory/>}/>
+            <Route path='/marketplace' element={<Marketplace/>}/>
+            <Route path='/games' element={<Games/>}/>
+            <Route path='/collections' element={<Collections/>}/>
+            <Route path='/settings' element={<Settings network={network} setNetwork={setNetwork} endpoint={endpoint} setEndpoint={setEndpoint} />}/>
+          </Routes>
+        </MainLayout>
+      </AppContext.Provider>
     )
-  }
 }
 export default App;
 
--- p3-react-app/src/components/Settings.js
+++ p3-react-app/src/components/Settings.js
@@ -1,9 +1,96 @@
-import React from 'react'
+import React from "react";
+import { Typography } from "@mui/material";
+import { useState } from "react";
+import { Select, MenuItem, InputLabel, Box, FormControl,  FormLabel, RadioGroup, FormControlLabel, Radio} from "@mui/material";
 
-function Settings() {
+const mainnetEndpoints = [
+  {
+    name: "EOSNATION",
+    url: "http://ultra.api.eosnation.io",
+  },
+  {
+    name: "EOSRIO",
+    url: "https://ultra.eosrio.io",
+  },
+  {
+    name: "CRYPTOLIONS",
+    url: "https://api.ultra.cryptolions.io/",
+  },
+  {
+    name: "EOSUSA",
+    url: "https://uos.eosusa.news",
+  }
+]
+
+const testnetEndpoints = [
+  {
+    name : 'EOSEOUL',
+    url: 'https://ultratest-api.eoseoul.io/'
+  },
+  {
+    name : 'EOSNATION',
+    url: 'http://ultratest.api.eosnation.io'
+  },
+  {
+    name : 'EOSRIO',
+    url: 'https://testnet.ultra.eosrio.io'
+  },
+  {
+    name : 'CRYPTOLIONS',
+    url: 'https://api.ultra-testnet.cryptolions.io'
+  }
+]
+
+function Settings(props) {
+
+  const [network, setNetwork] = useState(mainnetEndpoints)
+ 
+  const handleSelectChange = (event) => {
+    props.setEndpoint(event.target.value);
+  };
+  const handleNetworkChange = (event) => {
+    setNetwork((event.target.value == 'mainnet') ? mainnetEndpoints : testnetEndpoints)
+    props.setNetwork((event.target.value == 'mainnet') ? 'mainnet' : 'testnet')
+    props.setEndpoint((event.target.value == 'mainnet') ? mainnetEndpoints[0] : testnetEndpoints[0])
+  }
+
   return (
-    <div>Settings</div>
-  )
+    <div>
+      <Typography>Preferences</Typography>
+      <Box sx={{ minWidth: 120 }}>
+        <FormControl fullWidth>
+          <FormLabel id="demo-radio-buttons-group-label">Network</FormLabel>
+          <RadioGroup
+            aria-labelledby="demo-radio-buttons-group-label"
+            defaultValue="mainnet"
+            name="radio-buttons-group"
+            value={props.network}
+            onChange= {handleNetworkChange}
+          >
+            <FormControlLabel value= 'mainnet' control={<Radio />} label="Mainnet"/>
+            <FormControlLabel value= 'testnet' control={<Radio />} label="Testnet"/>
+
+          </RadioGroup>
+          </FormControl>
+          <FormControl fullWidth>
+          <InputLabel id="endpoints-label">Endpoint</InputLabel>
+          <Select
+            labelId="endpoints-label"
+            id="enpoint-select"
+            value={props.endpoint}
+            label="Endpoint"
+            onChange={handleSelectChange}
+          >
+             { network.map((element) => (
+              <MenuItem key={element.name} value={element.url}>
+                {element.name}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+      </Box>
+    </div>
+  );
 }
 
-export default Settings
+export default Settings;
