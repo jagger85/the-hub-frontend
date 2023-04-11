6f0d6effbdcab2a18950bc035e1a5da3bf0d3d6f
Jagger85 <jagger85.crypto@gmail.com>
2023-04-11 18:45:42 +0800

    FEAT: Dashboard now fetchs wallet info and display it

--- p3-react-app/src/components/Dashboard.js
+++ p3-react-app/src/components/Dashboard.js
@@ -1,9 +1,40 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { AppContext } from './App'
+import { useEffect, useState } from 'react'
+import { Typography } from '@mui/material'
+
+
 
 function Dashboard() {
+
+  const {wallet,network,endpoint} = useContext(AppContext)
+  const [info, setInfo] = useState(null)
+
+  useEffect(() =>{
+    fetch(`${endpoint}/v1/chain/get_account`, {
+      method: 'POST',
+      headers: {
+          'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      body: `{ "account_name": "${wallet}" }`
+  })
+      .then(res => res.json())
+      .then(data => {
+        setInfo(data)
+      })   
+  },[])
+
+
   return (
-    <div>Dashboard</div>
-  )
-}
+    info != null &&
+    <div>
+    <h1>Dashboard</h1>
+    
+    <Typography>Wallet : {info.account_name}</Typography>
+    <Typography>Balance : {info.core_liquid_balance}</Typography>
+    <Typography>Account created : {info.created}</Typography>
+    </div>
+    )
+  }
 
 export default Dashboard
