f0956d4cfce01bd94244007c03babb8962b46b9d
Jagger85 <jagger85.crypto@gmail.com>
2023-04-12 11:35:51 +0800

    FEAT: Transactions fetched but not showing on screen just on console

--- p3-react-app/src/components/Dashboard.js
+++ p3-react-app/src/components/Dashboard.js
@@ -1,40 +1,65 @@
-import React, { useContext } from 'react'
-import { AppContext } from './App'
-import { useEffect, useState } from 'react'
-import { Typography } from '@mui/material'
-
-
+import React, { useContext } from "react";
+import { AppContext } from "./App";
+import { useEffect, useState } from "react";
+import { Typography } from "@mui/material";
 
 function Dashboard() {
+  const { wallet, network, endpoint } = useContext(AppContext);
+  const [info, setInfo] = useState(null);
+  const [transactions, setTransactions] = useState(null);
 
-  const {wallet,network,endpoint} = useContext(AppContext)
-  const [info, setInfo] = useState(null)
+  useEffect(() => {
+    getAccountInfo();
+    getTransactions();
+  }, []);
 
-  useEffect(() =>{
+  function getAccountInfo() {
     fetch(`${endpoint}/v1/chain/get_account`, {
-      method: 'POST',
+      method: "POST",
       headers: {
-          'Content-Type': 'application/x-www-form-urlencoded'
+        "Content-Type": "application/x-www-form-urlencoded",
       },
-      body: `{ "account_name": "${wallet}" }`
-  })
-      .then(res => res.json())
-      .then(data => {
-        setInfo(data)
-      })   
-  },[])
-
-
-  return (
-    info != null &&
-    <div>
-    <h1>Dashboard</h1>
-    
-    <Typography>Wallet : {info.account_name}</Typography>
-    <Typography>Balance : {info.core_liquid_balance}</Typography>
-    <Typography>Account created : {info.created}</Typography>
-    </div>
-    )
+      body: `{ "account_name": "${wallet}" }`,
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setInfo(data);
+      })
+      .catch((error) => console.log("error", error));
   }
 
-export default Dashboard
+  function getTransactions() {
+    var raw = "";
+
+    var requestOptions = {
+      method: "POST",
+      body: raw,
+      redirect: "follow",
+    };
+
+    fetch(
+     `https://api.mainnet.ultra.io/v0/search/transactions?q=(auth:${wallet} OR receiver:${wallet})&sort=desc&limit=5&block_count=5000000&start_block=0&limit=5&cursor&with_reversible=true`,
+      requestOptions
+    )
+      .then((response) => response.json())
+      .then((result) => {
+        console.log(result)
+        setTransactions(result)
+      })
+      .catch((error) => console.log("error", error));
+  }
+
+  return (
+    info != null && (
+      <div>
+        <h1>Dashboard</h1>
+
+        <Typography>Wallet : {info.account_name}</Typography>
+        <Typography>Balance : {info.core_liquid_balance}</Typography>
+        <Typography>Account created : {info.created}</Typography>
+      </div>
+    )
+  );
+}
+
+export default Dashboard;
