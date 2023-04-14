0569305217aa2abf335149e886e2865c6f2a07f2
Jagger85 <jagger85.crypto@gmail.com>
2023-04-14 11:27:08 +0800

    FEAT: Implemented the zip function, now it works well and collections displays description for the uniq collections

--- p3-react-app/src/components/Collections/Collection.js
+++ p3-react-app/src/components/Collections/Collection.js
@@ -1,22 +1,36 @@
-import React from 'react'
-import { Paper, Typography } from '@mui/material'
-import { useEffect } from 'react'
+import React from "react";
+import { Paper, Typography } from "@mui/material";
+import { useEffect, useState,useCallback } from "react";
+import { unzip } from "../../scripts/unzipper";
+
 function Collection(props) {
+ const [metadata, setMetadata] = useState(null)
 
-    useEffect(()=>{
-
-    },[])
-
+useEffect(()=>{
+  const getMetadata = async () =>{
+    const data = await unzip(props.collection.meta_uris)
+    setMetadata(data)
+  }
+  getMetadata()
+  .catch(console.error)
+},[])
 
   return (
-    <Paper sx={{margin: 1, padding: 1}}>
-    <Typography>Id: {props.collection.id} </Typography>
-    <Typography>Minimum resell price: {props.collection.minimum_resell_price} </Typography>
-    <Typography>Max units: {props.collection.max_mintable_tokens}</Typography>
-    <Typography>Minted: {props.collection.minted_tokens_no}</Typography>
-    <Typography>Meta uris: {props.collection.meta_uris.map(e => e)}</Typography>
+
+    <Paper sx={{ margin: 1, padding: 1 }}>
+      <Typography>Id: {props.collection.id} </Typography>
+      <Typography>
+        Minimum resell price: {props.collection.minimum_resell_price}{" "}
+      </Typography>
+      <Typography>Max units: {props.collection.max_mintable_tokens}</Typography>
+      <Typography>Minted: {props.collection.minted_tokens_no}</Typography>
+      {metadata != null &&
+        <div>
+        <Typography>Description: {metadata.description}</Typography>
+        </div>
+      }
     </Paper>
-  )
+  );
 }
 
-export default Collection
+export default Collection;
--- p3-react-app/src/scripts/unzipper.js
+++ p3-react-app/src/scripts/unzipper.js
@@ -1,8 +1,7 @@
-export function unzip(url){
-    
-    let data = null
-
-    fetch(url) 
+export async function unzip(url) {
+    const JSZip = require("jszip");
+  
+ return fetch(url)
       .then(function (response) {
         if (response.status === 200 || response.status === 0) {
           return Promise.resolve(response.blob());
@@ -10,13 +9,10 @@ export function unzip(url){
           return Promise.reject(new Error(response.statusText));
         }
       })
-      .then(JSZip.loadAsync) 
-      .then(function (zip) {
-        console.log(zip.files)
-        manifest = zip.file('manifest.json').async('string')
-        console.log(manifest)
-        manifest.then(r => data = JSON.parse(r))
-
-        return data;
-    })
-}
+      .then(JSZip.loadAsync)
+      .then((zip) => zip.file("manifest.json").async("string"))
+      .then((r) => {
+          return JSON.parse(r)
+      
+      })
+  }
