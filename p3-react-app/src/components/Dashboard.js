56020ee6b2f08e9022e4921dabb23b3be48ae2de
Jagger85 <jagger85.crypto@gmail.com>
2023-04-14 14:11:04 +0800

    FEAT: Dashboard now relies on apicalls.js

--- p3-react-app/src/components/Dashboard.js
+++ p3-react-app/src/components/Dashboard.js
@@ -3,51 +3,26 @@ import { AppContext } from "./App";
 import { useEffect, useState } from "react";
 import { Typography } from "@mui/material";
 import TransactionsTable from "./Transactions/TransactionsTable";
+import { apiCalls } from "../scripts/apicalls";
 
 function Dashboard() {
-  const { wallet, network, endpoint } = useContext(AppContext);
+  const { wallet } = useContext(AppContext);
   const [info, setInfo] = useState(null);
   const [transactions, setTransactions] = useState(null);
 
   useEffect(() => {
-    getAccountInfo();
-    getTransactions();
-  }, []);
-
-  function getAccountInfo() {
-    fetch(`${endpoint}/v1/chain/get_account`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/x-www-form-urlencoded",
-      },
-      body: `{ "account_name": "${wallet}" }`,
-    })
-      .then((res) => res.json())
-      .then((data) => {
-        setInfo(data);
-      })
-      .catch((error) => console.log("error", error));
-  }
-
-  function getTransactions() {
-    var raw = "";
-
-    var requestOptions = {
-      method: "POST",
-      body: raw,
-      redirect: "follow",
+    const getInfo = async () => {
+      const data = await apiCalls.getWalletInfo(wallet);
+      setInfo(data);
+    };
+    const getTransactions = async () => {
+      const data = await apiCalls.getWalletTransactions(wallet);
+      setTransactions(data);
     };
 
-    fetch(
-      `https://api.mainnet.ultra.io/v0/search/transactions?q=(auth:${wallet} OR receiver:${wallet})&sort=desc&limit=100&block_count=5000000&start_block=0&limit=5&cursor&with_reversible=true`,
-      requestOptions
-    )
-      .then((response) => response.json())
-      .then((result) => {
-        setTransactions(result);
-      })
-      .catch((error) => console.log("error", error));
-  }
+    getInfo();
+    getTransactions();
+  }, []);
 
   return (
     info != null &&
