8f0f1a72b2b655e8d7cf38892542432de0353ac9
Jagger85 <jagger85.crypto@gmail.com>
2023-05-19 06:13:18 +0800

    FEAT: Added Login Layout

---
+++ p3-react-app/src/layouts/LoginLayout.jsx
@@ -0,0 +1,39 @@
+import { Box, Button, ThemeProvider } from '@mui/material';
+import { theme } from '../theme';
+import RegisterForm from '../components/Molecules/Forms/RegisterForm';
+import LoginForm from '../components/Molecules/Forms/LoginForm'
+import React from 'react';
+import { useState } from 'react';
+import background from '../assets/background.jpeg';
+
+function LoginLayout() {
+  
+  const[login, setLogin] = useState(true)
+  
+  const switchForm = () => {
+    setLogin(!login)
+  }
+
+
+  return (
+    <ThemeProvider theme={theme}>
+      
+    <Box
+        sx={{
+          backgroundImage: `url(${background})`,
+          height: '100vh',
+          width: '100vw',
+        }}>
+        {login ?
+          <LoginForm switch={() => switchForm()}/>:
+          <RegisterForm switch={() => switchForm()}/>
+        }
+
+        
+        </Box>
+      
+    </ThemeProvider>
+  );
+}
+
+export default LoginLayout;
