76b4eb2b1bc3fa569c6053f6d18bc46ef50769d8
Jagger85 <jagger85.crypto@gmail.com>
2023-05-20 15:38:55 +0800

    FEAT: Added bearer token,login fully operational, modified delete requests

--- p3-react-app/src/scripts/dataService.js
+++ p3-react-app/src/scripts/dataService.js
@@ -4,6 +4,8 @@ const a = axios.create({
   baseURL: 'https://the-hub-dt35.onrender.com/',
 });
 
+let config;
+
 export const dataService = {
   currency: '',
   user: '',
@@ -21,61 +23,72 @@ export const dataService = {
   },
 
   getPortfolios: async function () {
-    return await a.get(`user/portfolios/${this.user}`).then((res) => res.data.portfolios);
+    return await a.get(`user/portfolios/${this.user}`, config).then((res) => res.data.portfolios);
   },
 
   getPortfolio: async function (portfolioAlias) {
-    return await a.get(`user/${this.user}/${portfolioAlias}/`).then((res) => res.data.portfolio);
+    return await a.get(`user/${this.user}/${portfolioAlias}/`, config).then((res) => res.data.portfolio);
   },
 
   getWallet: async function (portfolioAlias, walletAlias) {
-    return await a.get(`user/${this.user}/${portfolioAlias}/${walletAlias}`).then((res) => res.data.wallets);
+    return await a.get(`user/${this.user}/${portfolioAlias}/${walletAlias}`, config).then((res) => res.data.wallets);
   },
 
   createUser: async function (username, email, password) {
-    return await a.post(`register`, { username: username, email: email, password: password  }).then((res) => console.log(res.data));
+    return await a
+      .post(`register`, { username: username, email: email, password: password })
+      .then((res) => console.log(res.data));
   },
 
   logInUser: async function (username, password) {
-    return a.post(`login`,{username: username, password: password})
-    .then(response => {
-      if(response.data.token) {
-        localStorage.setItem('token', response.data.token) 
-        dataService.setUser(username)
-        return response.data.message
-      }else{
-        return response.data.error
+    return a.post(`login`, { username: username, password: password }).then((response) => {
+      if (response.data.token) {
+        localStorage.setItem('token', response.data.token);
+        this.user = username;
+        config = {
+          headers: { Authorization: `Bearer ${localStorage.getItem('token')}` },
+        };
+        return response.data.message;
+      } else {
+        return response.data.error;
       }
-        
-      
-    })
+    });
   },
 
   getPortfolioWallets: async function (portfolioAlias) {
-    return await a.get(`/user/${this.user}/${portfolioAlias}/wallets`).then((res) => res.data.wallets);
+    return await a.get(`/user/${this.user}/${portfolioAlias}/wallets`, config).then((res) => res.data.wallets);
   },
 
   addPortfolio: async function (portfolioAlias) {
-    return await a.post(`user/portfolios/${this.user}`, { alias: portfolioAlias }).then((res) => res.data);
+    return await a.post(`user/portfolios/${this.user}`, { alias: portfolioAlias }, config).then((res) => res.data);
   },
 
   removePortfolio: async function (portfolioAlias) {
-    return await a.delete(`user/portfolios/${this.user}`, { data: { alias: portfolioAlias } }).then((res) => res.data);
+    return await a
+      .delete(`user/portfolios/${this.user}`, { ...config, data: { alias: portfolioAlias } })
+      .then((res) => res.data);
   },
 
   addWallet: async function (portfolioAlias, walletAlias, walletAddress) {
     return await a
-      .post(`user/portfolios/wallets/${this.user}`, {
-        alias: portfolioAlias,
-        walletAlias: walletAlias,
-        walletAddress: walletAddress,
-      })
+      .post(
+        `user/portfolios/wallets/${this.user}`,
+        {
+          alias: portfolioAlias,
+          walletAlias: walletAlias,
+          walletAddress: walletAddress,
+        },
+        config
+      )
       .then((res) => res.data);
   },
 
   removeWallet: async function (portfolioAlias, walletAlias) {
     return await a
-      .delete(`user/portfolios/wallets/${this.user}`, { data: { alias: portfolioAlias, walletAlias: walletAlias } })
+      .delete(`user/portfolios/wallets/${this.user}`, {
+        ...config,
+        data: { alias: portfolioAlias, walletAlias: walletAlias },
+      })
       .then((res) => res.data);
   },
 };
