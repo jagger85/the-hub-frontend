27fa7fee91dc3ef34201d5dc248ac64f33300e48
Jagger85 <jagger85.crypto@gmail.com>
2023-05-20 21:27:17 +0800

    FEAT: Using context

--- p3-react-app/src/components/Organisms/Dashboard.jsx
+++ p3-react-app/src/components/Organisms/Dashboard.jsx
@@ -1,31 +1,33 @@
-import React from "react";
-import { Typography, Grid } from "@mui/material";
-import TransactionsTable from "../Molecules/Transactions/TransactionsTable";
-import WalletInfo from "../Molecules/WalletInfo/WalletInfo";
-import { styles as stl } from "./PagesStyle";
-import { dataService } from "../../scripts/dataService";
-import NoData from "../Atoms/NoData";
-function Dashboard() {
-  return (
-      <Grid container sx={stl.container}>
-        <Grid item xs={12} sx={stl.titleContainer}>
-          <Typography variant="h3">
-            Dashboard
-          </Typography>
-        </Grid>
-        <Grid item xs={5} sx={stl.section}>
-          <WalletInfo />
-        </Grid>
-        <Grid item xs={12} sx={stl.section}>
+import { Typography, Grid } from '@mui/material';
+import TransactionsTable from '../Molecules/Transactions/TransactionsTable';
+import WalletInfo from '../Molecules/WalletInfo/WalletInfo';
+import { styles as stl } from './PagesStyle';
+import NoData from '../Atoms/NoData';
+import { MyContext } from '../../utils/MyContexProvider'
+import { useContext } from 'react';
 
-        {dataService.portfolios[0].wallets ?
-          dataService.portfolios[0].wallets.map( x => {
-            return <TransactionsTable wallet={x.address} alias={x.alias} />
-          }):
-          <NoData/>
-        }
-        </Grid>
+function Dashboard() {
+
+  const { state } = useContext(MyContext);
+
+  return (
+    <Grid container sx={stl.container}>
+      <Grid item xs={12} sx={stl.titleContainer}>
+        <Typography variant='h3'>Dashboard</Typography>
       </Grid>
+      <Grid item xs={5} sx={stl.section}>
+        <WalletInfo />
+      </Grid>
+      <Grid item xs={12} sx={stl.section}>
+        {state.portfolios.length > 0 ? (
+          state.portfolios[0].wallets.map((x) => {
+            return <TransactionsTable wallet={x.address} alias={x.alias} />;
+          })
+        ) : (
+          <NoData />
+        )}
+      </Grid>
+    </Grid>
   );
 }
 
