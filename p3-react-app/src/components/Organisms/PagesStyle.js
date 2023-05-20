5b7df9f335a9663d6151dd3e23fdaa1075e1952a
Jagger85 <jagger85.crypto@gmail.com>
2023-05-20 17:00:58 +0800

    FIX: Changed extension to js

---
+++ p3-react-app/src/components/Organisms/PagesStyle.js
@@ -0,0 +1,13 @@
+export const styles = {
+    container : {
+    margin:2,
+    },
+    titleContainer:{
+    marginBottom: 3,
+    },
+    section: {
+    marginRight : 5,
+    marginTop: 2
+
+    }
+}
