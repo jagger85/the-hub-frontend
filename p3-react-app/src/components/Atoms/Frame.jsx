dc8438167252fb31ae3337388558303ab1afd1ed
Jagger85 <jagger85.crypto@gmail.com>
2023-05-25 03:27:53 +0800

    CHORE: Some testing with uniqs images and styles

--- p3-react-app/src/components/Atoms/Frame.jsx
+++ p3-react-app/src/components/Atoms/Frame.jsx
@@ -3,19 +3,16 @@ import { Box } from '@mui/material'
 import { colors, doubleBorder } from '../../theme'
 
 const stl = {
-    position: 'absolute',
-    right:1300,
-    top:400,
     background: colors.background[800],         //'#3C3846',
     borderRadius: 0.5,
-    width: 200,
-    height: 200,
     boxShadow: doubleBorder
   }
 
-function Frame() {
+function Frame({children}) {
   return (
-    <Box sx={stl}> </Box>
+    <Box sx={stl}> 
+    {children}
+    </Box>
   )
 }
 
--- p3-react-app/src/components/TestArea/TestArea.jsx
+++ p3-react-app/src/components/TestArea/TestArea.jsx
@@ -2,12 +2,13 @@ import React from 'react';
 import { Box } from '@mui/material';
 import ZipViewer from './ZipViewer';
 import Frame from '../Atoms/Frame';
+import UniqImage from '../Atoms/UniqImage';
 function Games() {
 
 
   return (
     <Box>
-      <Frame/>
+      <UniqImage></UniqImage>
     </Box>
   );
 }
