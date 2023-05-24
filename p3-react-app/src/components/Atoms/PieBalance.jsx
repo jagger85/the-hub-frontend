1476e688f96a09b31cfcaf795871cde21d121950
Jagger85 <jagger85.crypto@gmail.com>
2023-05-25 03:22:52 +0800

    FEAT: Some chart testing

---
+++ p3-react-app/src/components/Atoms/PieBalance.jsx
@@ -0,0 +1,64 @@
+import React, { useEffect, useState } from 'react';
+import ReactEcharts from 'echarts-for-react';
+import { colors } from '../../theme';
+
+function PieBalance(props) {
+
+  const [data, setData] = useState(props.balances);
+
+  let options = {
+    color: [
+      colors.purple[500],
+      colors.purple[800],
+      colors.purple[300],
+      colors.purple[400],
+      colors.purple[100],
+      colors.purple[200],
+      colors.purple[100],
+      colors.purple[100],
+      colors.purple[100],
+      colors.purple[100],
+    ],
+
+    tooltip: {
+      trigger: 'item',
+      backgroundColor: colors.background[500],
+      borderColor: colors.background[800],
+      textStyle: {
+        color: 'white',
+      },
+    },
+    legend: {
+      orient: 'vertical',
+      bottom: '15%',
+      left: 10,
+      icon: 'rect',
+      textStyle: {
+        color: 'white',
+      },
+    },
+    series: [
+      {
+        name: 'Balance',
+        type: 'pie',
+        radius: ['45%', '70%'],
+        avoidLabelOverlap: false,
+        label: {
+          show: false,
+          position: 'center',
+        },
+        data: data
+      },
+      
+    ],
+   
+  };
+
+  useEffect(()=>{
+    setData(props.balances)
+  },[props])
+  
+  return <ReactEcharts style={{height: '120px' }} option={options} />;
+}
+
+export default PieBalance;
--- p3-react-app/src/components/Molecules/WalletInfo/WalletInfo.jsx
+++ p3-react-app/src/components/Molecules/WalletInfo/WalletInfo.jsx
@@ -4,33 +4,52 @@ import { apiCalls } from '../../../utils/apicalls';
 import { Paper, Typography, Grid, Divider } from '@mui/material';
 import { styles } from './WalletInfoStyle';
 import NoData from '../../Atoms/NoData';
+import { v4 as uuid } from 'uuid';
+import PieBalance from '../../Atoms/PieBalance';
+
 
 /**
  * @returns - A MUI Paper with a grid inside that shows info related to the wallet
  */
-function WalletInfo() {
-  const wallet = localStorage.getItem('wallet');
-  const [info, setInfo] = useState(null);
+function WalletInfo(props) {
+
+  const [info, setInfo] = useState([]);
+  const [pieInfo, setPieInfo] = useState([])
 
   useEffect(() => {
     const getInfo = async () => {
-      const data = await apiCalls.getWalletInfo(wallet);
-      setInfo(data);
+      let wallets = [];
+      let pieInfo = [];
+      for (let wallet of props.portfolio.wallets) {
+        const data = await apiCalls.getWalletInfo(wallet.address);
+        wallets = [...wallets, data];
+        pieInfo = [...pieInfo, {value: parseInt(data.core_liquid_balance), name: wallet.alias}]
+      }
+      setInfo(wallets);
+      setPieInfo(pieInfo)
     };
+    console.count('info');
     getInfo();
-  }, []);
+  }, [props]);
 
-  return info != null ? (
+  return info.length != 0 ? (
     <Paper sx={styles.paper}>
       <Grid container>
         <Grid item xs={12}>
-          <Typography variant='h5'>Balance</Typography>
+          <Typography variant='h5'>Net worth</Typography>
           <Divider />
         </Grid>
-        <Grid item xs={5}>
-          <Typography>Balance: {parseInt(info.core_liquid_balance).toFixed()}</Typography>
+        <Grid item sx={styles.balances} key={uuid()} xs={5}>
+        {info.map((wallet) => {
+          return (
+              <Typography key={uuid()}>Balance: {parseInt(wallet.core_liquid_balance).toFixed()}</Typography>
+              );
+            })}
+            </Grid>
+            <Grid item xs={8} >
+        <PieBalance balances={pieInfo}/>
         </Grid>
-      </Grid>
+        </Grid>
     </Paper>
   ) : (
     <NoData text='No data' />
