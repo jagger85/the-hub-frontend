52ad9814c96d19f19620ac5de8a42ec97f6246bd
Jagger85 <andresfonsortega@yahoo.es>
2023-06-16 18:36:21 +0800

    Feat: Modified buy transaction

--- src/components/Molecules/Transactions/Buy.jsx
+++ src/components/Molecules/Transactions/Buy.jsx
@@ -1,59 +1,49 @@
-import React from 'react'
-import { Accordion, AccordionSummary, AccordionDetails } from '@mui/material'
-import { Typography } from '@mui/material'
-import { v4 as uuid } from 'uuid'
-import { styles } from './TransactionStyle'
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore'
-import { useState } from 'react'
+import React from 'react';
+import { Accordion, AccordionSummary, AccordionDetails } from '@mui/material';
+import { Typography } from '@mui/material';
+import { v4 as uuid } from 'uuid';
+import { styles } from './TransactionStyle';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import { useState } from 'react';
+import { colors } from '../../../theme';
+import moment from 'moment';
 
 function Buy(props) {
-    const [expanded, setExpanded] = useState(false)
+  const [expanded, setExpanded] = useState(false);
 
-    const handleChange = (panel) => (event, isExpanded) => {
-        setExpanded(isExpanded ? panel : false)
-    }
+  const handleChange = (panel) => (event, isExpanded) => {
+    setExpanded(isExpanded ? panel : false);
+  };
+  console.log(props)
+  return (
+    <Accordion
+      key={uuid()}
+      sx={styles.transactionContainer}
+      expanded={expanded === 'panel1'}
+      onChange={handleChange('panel1')}>
+      <AccordionSummary expandIcon={<ExpandMoreIcon />} aria-controls='panel1bh-content' id='panel1bh-header'>
+        <Typography variant='h7medium' sx={{ width: '33%', flexShrink: 0, color: colors.green[100] }}>
+          {props.transaction.act.name.toUpperCase()}
+          </Typography>
+        <Typography sx={{ color: 'text.secondary' }}>I am an accordion</Typography>
+      </AccordionSummary>
 
-    return (
-        <Accordion
-            key={uuid()}
-            sx={styles.transactionContainer}
-            expanded={expanded === 'panel1'}
-            onChange={handleChange('panel1')}
-        >
-            <AccordionSummary
-                expandIcon={<ExpandMoreIcon />}
-                aria-controls="panel1bh-content"
-                id="panel1bh-header"
-            >
-                <Typography sx={{ width: '33%', flexShrink: 0 }}>
-                    {props.transaction.act.name}
-                </Typography>
-                <Typography sx={{ color: 'text.secondary' }}>
-                    I am an accordion
-                </Typography>
-            </AccordionSummary>
-
-            <AccordionDetails>
-                <Typography>Type : {props.transaction.act.name}</Typography>
-                <Typography>
-                    Token id : {props.transaction.act.data.buy.token_id}
-                </Typography>
-                {/** 
+      <AccordionDetails>
+        <Typography>Type : {props.transaction.act.name}</Typography>
+        <Typography>Token id : {props.transaction.act.data.buy.token_id}</Typography>
+        {/** 
     <Typography>Buyer : {e.act.data.buy.buyer}</Typography>
   */}
-                <Typography>
-                    Receiver : {props.transaction.act.data.buy.receiver}
-                </Typography>
-                <Typography>
-                    Price : {props.transaction.act.data.buy.max_price}
-                </Typography>
-                {/** 
+        <Typography>Receiver : {props.transaction.act.data.buy.receiver}</Typography>
+        <Typography>Price : {props.transaction.act.data.buy.max_price}</Typography>
+        <Typography>Block time : {moment(props.transaction.block_time).format('D MMM YYYY - H:mm')}</Typography>
+        {/** 
   <Typography> Promoter id : {e.act.data.buy.promoter_id}
   </Typography>
 */}
-            </AccordionDetails>
-        </Accordion>
-    )
+      </AccordionDetails>
+    </Accordion>
+  );
 }
 
-export default Buy
+export default Buy;
--- src/components/Molecules/WalletInfo/WalletInfo.jsx
+++ src/components/Molecules/WalletInfo/WalletInfo.jsx
@@ -1,78 +1,64 @@
-import React from 'react'
-import { useEffect, useState } from 'react'
-import { apiCalls } from '../../../utils/apicalls'
-import { Paper, Typography, Grid, Divider } from '@mui/material'
-import { styles } from './WalletInfoStyle'
-import NoData from '../../Atoms/NoData'
-import { v4 as uuid } from 'uuid'
-import PieBalance from '../../Atoms/PieBalance'
-import { colors } from '../../../theme'
+import React from 'react';
+import { useEffect, useState } from 'react';
+import { apiCalls } from '../../../utils/apicalls';
+import { Paper, Typography, Grid, Divider } from '@mui/material';
+import { styles } from './WalletInfoStyle';
+import NoData from '../../Atoms/NoData';
+import { v4 as uuid } from 'uuid';
+import PieBalance from '../../Atoms/PieBalance';
+import { colors } from '../../../theme';
 
 /**
  * @returns - A MUI Paper with a grid inside that shows info related to the wallet
  */
 function WalletInfo(props) {
-    const [info, setInfo] = useState([])
-    const [pieInfo, setPieInfo] = useState([])
+  const [info, setInfo] = useState([]);
+  const [pieInfo, setPieInfo] = useState([]);
 
-    useEffect(() => {
-        const getInfo = async () => {
-            let wallets = []
-            let pieInfo = []
-            if (props.portfolio.wallets != undefined) {
-                for (let wallet of props.portfolio.wallets) {
-                    const data = await apiCalls.getWalletInfo(wallet.address)
-                    wallets = [...wallets, data]
-                    pieInfo = [
-                        ...pieInfo,
-                        {
-                            value: parseInt(data.core_liquid_balance),
-                            name: wallet.alias,
-                        },
-                    ]
-                }
-                setInfo(wallets)
-                setPieInfo(pieInfo)
-            }
+  useEffect(() => {
+    const getInfo = async () => {
+      let wallets = [];
+      let pieInfo = [];
+      if (props.portfolio.wallets != undefined) {
+        for (let wallet of props.portfolio.wallets) {
+          const data = await apiCalls.getWalletInfo(wallet.address);
+          wallets = [...wallets, data];
+          pieInfo = [...pieInfo, { value: parseInt(data.core_liquid_balance), name: wallet.alias }];
         }
-        console.count('info')
-        getInfo()
-    }, [props])
+        setInfo(wallets);
+        setPieInfo(pieInfo);
+      }
+    };
+    console.count('info');
+    getInfo();
+  }, [props]);
 
-    return info.length != 0 ? (
-        <Paper sx={styles.paper}>
-            <Grid container>
-                <Grid item xs={12}>
-                    <Typography
-                        variant="h5medium"
-                        color={colors.background[200]}
-                    >
-                        Net worth
-                    </Typography>
-                    <Divider />
-                </Grid>
-                <Grid item sx={styles.balances} key={uuid()} xs={5}>
-                    {info.map((wallet) => {
-                        return (
-                            <Typography
-                                variant="h7light"
-                                color={colors.background[200]}
-                                key={uuid()}
-                            >
-                                Balance:{' '}
-                                {parseInt(wallet.core_liquid_balance).toFixed()}
-                            </Typography>
-                        )
-                    })}
-                </Grid>
-                <Grid item xs={8}>
-                    <PieBalance balances={pieInfo} />
-                </Grid>
-            </Grid>
-        </Paper>
-    ) : (
-        <NoData text="No data" />
-    )
+  return info.length != 0 ? (
+    <Grid container sx={styles.container}>
+      <Paper sx={styles.paper}>
+        <Grid item xs={12}>
+          <Typography variant='h5medium' color={colors.background[200]}>
+            Net worth
+          </Typography>
+          <Divider />
+        </Grid>
+        <Grid item sx={styles.balances} key={uuid()} xs={5}>
+          {info.map((wallet) => {
+            return (
+              <Typography variant='h7light' color={colors.background[200]} key={uuid()}>
+                Balance: {parseInt(wallet.core_liquid_balance).toFixed()}
+              </Typography>
+            );
+          })}
+        </Grid>
+        <Grid item xs={12}>
+          <PieBalance balances={pieInfo} />
+        </Grid>
+      </Paper>
+    </Grid>
+  ) : (
+    <NoData text='No data' />
+  );
 }
 
-export default WalletInfo
+export default WalletInfo;
